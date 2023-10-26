7ce88bce39ffa7dbdf01aa587b7716eb50029b55
Hanna RIzza Malana <hannamalana98@gmail.com>
2023-10-26 19:25:03 +0800

    update

--- lib/MAIN PROFILE/BOTTOM NAVIGATION BAR/bottomnavbar.dart
+++ lib/MAIN PROFILE/BOTTOM NAVIGATION BAR/bottomnavbar.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
-//bottom
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
