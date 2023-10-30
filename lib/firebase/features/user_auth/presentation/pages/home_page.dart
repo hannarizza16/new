8623cc0d0fa7e5f98efbe4e9ea79d2bcb716ef42
Psynaptix <evardomecharlesjaesy@gmail.com>
2023-10-31 01:13:47 +0800

    ver1.2
    *Fontstyle feature added
    *Warning message when the back button is pressed , warning for user to logout

--- lib/firebase/features/user_auth/presentation/pages/home_page.dart
+++ lib/firebase/features/user_auth/presentation/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:first_project/bottom_navigations/home_bottom.dart';
 import 'package:first_project/bottom_navigations/newsfeed_bottom.dart';
 import 'package:first_project/bottom_navigations/profile_bottom.dart';
 import 'package:first_project/bottom_navigations/challenges_bottom.dart';
+import 'package:first_project/firebase/features/user_auth/presentation/pages/login_page.dart';
 import 'package:first_project/firebase/features/user_auth/presentation/widgets/draggable_fab.dart';
 import 'package:first_project/firebase/features/user_auth/presentation/widgets/sidebar.dart';
 import 'package:first_project/extension/bottom_nav_ext.dart';
@@ -13,6 +14,7 @@ import 'package:first_project/subject/game_dev.dart';
 import 'package:first_project/subject/machine_dev.dart';
 import 'package:first_project/subject/web_dev.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import '../../../../../ai_storage/ai_main.dart';
 import 'package:flutter/material.dart';
@@ -35,39 +37,55 @@ class _MainHomePageState extends State<MainHomePage> {
 
   int selectedCurrentIndex = 0;
 
+  Future<void> _handleLogout() async {
+    final shouldLogout = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Logout'),
+          content: Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Yes'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('No'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (shouldLogout ?? false) {
+      Fluttertoast.showToast(
+        msg: "Logged out", // Toast message when the user logs out
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+      // Navigating to another screen on logout
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(), // Replace 'YourOtherScreen()' with the screen you want to navigate to on logout
+        ),
+      );
+    }
+  }
+
   Future<bool> _onWillPop() async {
     if (selectedCurrentIndex == 0) {
-      final shouldLogout = await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Logout'),
-            content: Text('Are you sure you want to log out?'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: Text('Yes'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-                child: Text('No'),
-              ),
-            ],
-          );
-        },
-      );
-
-      if (shouldLogout ?? false) {
-        // Perform logout actions here
-        // For example:
-        // Your logout logic, clearing tokens, etc.
-      }
-
-      return shouldLogout ?? false;
+      await _handleLogout();
+      // Return true to handle back button press in case of not logging out
+      return false;
     }
     return true;
   }
--- lib/main_profile/languages/constant.dart
+++ lib/main_profile/languages/constant.dart
@@ -12,24 +12,7 @@ const codex = {
     Categories.advanced: {},
   },
   Languages.cplusplus: {
-    Categories.beginner: {
-      0: {
-        'topic': 'Introduction to C++',
-        'heading': 'What is C++?',
-        'body': 'C++ is a cross-platform language that can be used to create high-performance applications. '
-            'C++ was developed by Bjarne Stroustrup, as an extension to the C language. '
-            'C++ gives programmers a high level of control over system resources and memory. '
-            'The language was updated 4 major times in 2011, 2014, 2017, and 2020 to C++11, C++14, C++17, C++20.'
-      },
-      1: {
-        'topic': 'Introduction to C+++',
-        'heading': 'What is C++?',
-        'body': 'C++ is a cross-platform language that can be used to create high-performance applications. '
-            'C++ was developed by Bjarne Stroustrup, as an extension to the C language. '
-            'C++ gives programmers a high level of control over system resources and memory. '
-            'The language was updated 4 major times in 2011, 2014, 2017, and 2020 to C++11, C++14, C++17, C++20.'
-      }
-    },
+    Categories.beginner: {},
     Categories.intermediate: {},
     Categories.advanced: {},
   },
@@ -41,18 +24,98 @@ const codex = {
   Languages.java: {
     Categories.beginner: {
       0: {
-        'topic': 'Introduction to Java',
+        'topic': '1.     Introduction to Java',
         'heading': 'What is Java?',
         'body': "Java is a popular programming language, created in 1995."
             "\nIt is owned by Oracle, and more than 3 billion devices run Java."
             "\n\nIt is used for:"
-            "\n • Mobile applications (specially Android apps)"
-            "\n • Desktop applications"
-            "\n • Web applications"
-            "\n • Web servers and application servers"
-            "\n • GamesDatabase connection"
-
+            "\n   • Mobile applications (specially Android apps)"
+            "\n   • Desktop applications"
+            "\n   • Web applications"
+            "\n   • Web servers and application servers"
+            "\n   • GamesDatabase connection"
       },
+      1: {
+        'topic': '2.     Get Started',
+        'heading': '',
+        'body': ""
+      },
+      2: {
+        'topic': '3.     Syntax',
+        'heading': '',
+        'body': ""
+      },
+      3: {
+        'topic': '4.     Output',
+        'heading': '',
+        'body': ""
+      },
+      4: {
+        'topic': '     4.1     Print Text',
+        'heading': '',
+        'body': ""
+      },
+      5: {
+        'topic': '     4.2     Print Numbers',
+        'heading': '',
+        'body': ""
+      },
+      6: {
+        'topic': '5.     Comments',
+        'heading': '',
+        'body': ""
+      },
+      7: {
+        'topic': '6.     Variables',
+        'heading': '',
+        'body': ""
+      },
+      8: {
+        'topic': '     6.1     Print Variables',
+        'heading': '',
+        'body': ""
+      },
+      9: {
+        'topic': '     6.2     Declare Multiple Variables',
+        'heading': '',
+        'body': ""
+      },
+      10: {
+        'topic': '     6.3     Identifiers',
+        'heading': '',
+        'body': ""
+      },
+      11: {
+        'topic': '7.    Java Data Types',
+        'heading': '',
+        'body': ""
+      },
+      12: {
+        'topic': '     7.1     Data Types',
+        'heading': '',
+        'body': ""
+      },
+      13: {
+        'topic': '     7.2     Numbers',
+        'heading': '',
+        'body': ""
+      },
+      14: {
+        'topic': '     7.3     Booleans',
+        'heading': '',
+        'body': ""
+      },
+      15: {
+        'topic': '     7.4     Characters',
+        'heading': '',
+        'body': ""
+      },
+      16: {
+        'topic': '     7.5     Non-Primitive Types',
+        'heading': '',
+        'body': ""
+      },
+
     },
     Categories.intermediate: {},
     Categories.advanced: {},
