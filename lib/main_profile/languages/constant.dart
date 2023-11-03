d25e5b6686e702f2acf26d5ea272af9b098c91b4
Psynaptix <evardomecharlesjaesy@gmail.com>
2023-11-04 00:39:28 +0800

    *Fix Image attachment NULL ERROR *Looping of image
    ERROR

--- lib/main_profile/languages/constant.dart
+++ lib/main_profile/languages/constant.dart
@@ -28,9 +28,7 @@ const codex = {
         'subTopic': [
           {
             'heading': 'What is Java?',
-            'attachment' : 'assets/jrizal.jpg',
-            'body':
-                "Java is a popular programming language, created in 1995. "
+            'body': "Java is a popular programming language, created in 1995. "
                 "It is owned by Oracle, and more than 3 billion devices run Java."
                 "\n\nIt is used for:"
                 "\n\n   • Mobile applications (specially Android apps)"
@@ -41,8 +39,7 @@ const codex = {
           },
           {
             'heading': 'Why Use Java?',
-            'body':
-                "\n   • Java works on different platforms \n     (Windows, Mac, Linux, Raspberry Pi, etc.)"
+            'body': "\n   • Java works on different platforms \n     (Windows, Mac, Linux, Raspberry Pi, etc.)"
                 "\n   • It is one of the most popular programming \n      language in the world "
                 "\n   • It has a large demand in the current job market"
                 "\n   • It is easy to learn and simple to use "
@@ -56,6 +53,86 @@ const codex = {
                 "\n      for programmers to switch to Java or vice versa "
           }
         ]
+      },
+      1: {
+        'topic': '2.     Get Started',
+        'subTopic': [
+          {
+            'heading': 'Java Install',
+            'attachment': 'assets/java_lessons_image/get_started1.png',
+            'body': "Some PCs might have Java already installed."
+                "\n\nTo check if you have Java installed on a Windows PC, search in the start bar for Java or type the following in Command Prompt (cmd.exe):"
+
+          },
+          {
+            'attachment': 'assets/java_lessons_image/get_started2.jpg',
+            'body':
+                "If Java is installed, you will see something like this (depending on version):"
+          },
+          {
+            'body':
+                "If you do not have Java installed on your computer, you can download it for free at oracle.com"
+                "\n\nNOTE: In this tutorial, we will write Java code in a text editor."
+                    " However, it is possible to write Java in an Integrated Development Environment, such as IntelliJ IDEA, Netbeans or Eclipse, which are particularly useful when managing larger collections of Java files."
+          },
+          {
+            'heading': 'Setup For Windows',
+            'attachment': 'assets/java_lessons_image/step1_get_started.jpg',
+            'body': "To install Java on Windows: "
+                "\n\nTo check if you have Java installed on a Windows PC, search in the start bar for Java or type the following in Command Prompt (cmd.exe):"
+                "\n\n 1. Go to System Properties (Can be found on Control Panel > System and Security > System > Advanced System Settings)."
+          },
+
+          {
+            'attachment': 'assets/java_lessons_image/step2_get_started.jpg',
+            'body': "\n2. Click on the Environment variables button under the Advanced tab."
+          },
+          {
+            'attachment': 'assets/java_lessons_image/step3_get_started.jpg',
+            'body': "\n3. Then, select the Path variable in System variables and click on the Edit button."
+          },
+          {
+            'attachment': 'assets/java_lessons_image/step4_get_started.jpg',
+            'body': "\n4. Click on the New button and add the path where Java is installed, followed by -bin By default, Java is installed in C:-Program Files-Java-jdk-11.0.1 \n(If nothing else was specified when you installed it).\nIn that case, You will have to add a new path with: C:-Program Files-Java-jdk-11.0.1-bin"
+                " then, click OK, and save the settings."
+          },
+          {
+            'attachment': 'assets/java_lessons_image/step5_get_started.jpg',
+            'body': "\n5. At last, open Command Prompt (cmd.exe) and type java -version to see if Java is running on your machine."
+          },
+          {
+            'attachment': 'assets/java_lessons_image/step6_get_started.jpg',
+            'body': "\nIf Java was successfully installed, you will see something like this (depending on version):"
+          }
+        ]
+      },
+      2: {
+        'topic': '3.     Syntax',
+        'subTopic': [
+          {'heading': '', 'body': " "}
+        ]
+      },
+      3: {'topic': '4.     Output', 'heading': '', 'body': ""},
+      4: {'topic': '     4.1     Print Text', 'heading': '', 'body': ""},
+      5: {'topic': '     4.2     Print Numbers', 'heading': '', 'body': ""},
+      6: {'topic': '5.     Comments', 'heading': '', 'body': ""},
+      7: {'topic': '6.     Variables', 'heading': '', 'body': ""},
+      8: {'topic': '     6.1     Print Variables', 'heading': '', 'body': ""},
+      9: {
+        'topic': '     6.2     Declare Multiple Variables',
+        'heading': '',
+        'body': ""
+      },
+      10: {'topic': '     6.3     Identifiers', 'heading': '', 'body': ""},
+      11: {'topic': '7.    Java Data Types', 'heading': '', 'body': ""},
+      12: {'topic': '     7.1     Data Types', 'heading': '', 'body': ""},
+      13: {'topic': '     7.2     Numbers', 'heading': '', 'body': ""},
+      14: {'topic': '     7.3     Booleans', 'heading': '', 'body': ""},
+      15: {'topic': '     7.4     Characters', 'heading': '', 'body': ""},
+      16: {
+        'topic': '     7.5     Non-Primitive Types',
+        'heading': '',
+        'body': ""
       },
     },
     Categories.intermediate: {},
