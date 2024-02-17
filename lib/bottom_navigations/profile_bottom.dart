7a4d4ee5ae7b86b4041851b3a846a1056ec046b2
Hanna RIzza Malana <hannamalana98@gmail.com>
2024-02-17 23:15:15 +0800

    profile_pic

--- lib/bottom_navigations/profile_bottom.dart
+++ lib/bottom_navigations/profile_bottom.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile PIC',
-        style: TextStyle(
-          fontSize: 60,
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            radius: 31,
+            fontsize: 21,
+            img: 'https://avatars.githubusercontent.com/u/37553901?v=4',
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class ProfilePicture extends StatelessWidget {
+  final String name;
+  final double radius;
+  final double fontsize;
+  final String img;
+
+  const ProfilePicture({
+    required this.name,
+    required this.radius,
+    required this.fontsize,
+    required this.img,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: radius,
+          backgroundImage: NetworkImage(img),
+        ),
+        SizedBox(height: 10),
+        Text(
+          name,
+          style: TextStyle(fontSize: fontsize),
+        ),
+      ],
     );
   }
 }
