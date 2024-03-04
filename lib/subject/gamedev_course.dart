b52a5418f5cefa814f7db608050ec83a86e829ae
Psynaptix <evardomecharlesjaesy@gmail.com>
2024-03-05 00:59:56 +0800

    PHP-ADV-MARK

--- lib/subject/gamedev_course.dart
+++ lib/subject/gamedev_course.dart
@@ -2,13 +2,6 @@ import 'package:first_project/enums/enums.dart';
 
 const gameCourse = {
   SubjectSection.gamedev: {
-    0: {
-      'topic': '1.     Introduction to Game Development',
-      'subTopic' : [{
-        'heading' : 'Game Development',
-        'subheading' : "Hell"
-      }
-      ]
-    },
+
   }
 };
