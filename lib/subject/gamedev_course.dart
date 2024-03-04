d7020e57fbad7bfb0da54483b8854a903756d7a2
Jaesy <evardomecharlesjaesy@gmail.com>
2024-03-05 00:48:27 +0800

    DONE

--- lib/subject/gamedev_course.dart
+++ lib/subject/gamedev_course.dart
@@ -2,6 +2,13 @@ import 'package:first_project/enums/enums.dart';
 
 const gameCourse = {
   SubjectSection.gamedev: {
-    0: {},
+    0: {
+      'topic': '1.     Introduction to Game Development',
+      'subTopic' : [{
+        'heading' : 'Game Development',
+        'subheading' : "Hell"
+      }
+      ]
+    },
   }
 };
