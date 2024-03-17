23c7500ad07f0b5d5d93061b20c3688655bede49
Hanna RIzza Malana <hannamalana98@gmail.com>
2024-03-17 22:36:21 +0800

    put padding, background ok, title ok

--- lib/professors_profile/bottom_professor/students_bottom.dart
+++ lib/professors_profile/bottom_professor/students_bottom.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_project/gradient_background.dart';
 
 class StudentBottomScreen extends StatefulWidget {
   @override
@@ -9,7 +10,15 @@ class StudentBottomScreen extends StatefulWidget {
 
 class _StudentBottomScreenState extends State<StudentBottomScreen> {
   String? currentUserEmail = FirebaseAuth.instance.currentUser?.email;
-  final List<String> _sections = ['All', '101P', '102P','202P','401P','501P','801P']; // Add specific sections
+  final List<String> _sections = [
+    'All',
+    '101P',
+    '102P',
+    '202P',
+    '401P',
+    '501P',
+    '801P'
+  ]; // Add specific sections
   final List<int> _yearLevels = [0, 1, 2, 3, 4]; // Add specific year levels
 
   String _selectedSection = 'All'; // Default selected section
@@ -18,7 +27,29 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: Stack(children: [
+      // Layer 1: Gradient Container
+      const GradientContainer(
+        child: SizedBox.expand(), // Expand to fill the whole screen
+      ),
+      // Layer 2: Image
+
+      Positioned(
+        left: 0, // Align left
+        top: 0, // Align top
+        right: 0, // Align right
+        bottom: 0, // Align bottom
+        child: Opacity(
+          opacity: 0.2, // Specify the opacity value here (0.0 - 1.0)
+          child: Image.asset(
+            'assets/overlay/2.jpg', // Replace with your image path
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      // Layer 2: Image
+      // Layer 3: Other Widgets
+      Column(
         children: [
           Container(
             alignment: Alignment.center,
@@ -65,7 +96,8 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
                   items: _yearLevels.map((yearLevel) {
                     return DropdownMenuItem<int>(
                       value: yearLevel,
-                      child: Text(yearLevel == 0 ? 'All' : yearLevel.toString()),
+                      child:
+                          Text(yearLevel == 0 ? 'All' : yearLevel.toString()),
                     );
                   }).toList(),
                 ),
@@ -76,31 +108,35 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
             child: StreamBuilder<QuerySnapshot>(
               stream: _selectedSection == 'All' && _selectedYearLevel == 0
                   ? FirebaseFirestore.instance
-                  .collection('students')
-                  .where('selected_teacher', isEqualTo: currentUserEmail)
-                  .orderBy('last_name') // Sort by last name
-                  .snapshots()
+                      .collection('students')
+                      .where('selected_teacher', isEqualTo: currentUserEmail)
+                      .orderBy('last_name') // Sort by last name
+                      .snapshots()
                   : _selectedSection == 'All'
-                  ? FirebaseFirestore.instance
-                  .collection('students')
-                  .where('selected_teacher', isEqualTo: currentUserEmail)
-                  .where('year_level', isEqualTo: _selectedYearLevel)
-                  .orderBy('last_name') // Sort by last name
-                  .snapshots()
-                  : _selectedYearLevel == 0
-                  ? FirebaseFirestore.instance
-                  .collection('students')
-                  .where('selected_teacher', isEqualTo: currentUserEmail)
-                  .where('section', isEqualTo: _selectedSection)
-                  .orderBy('last_name') // Sort by last name
-                  .snapshots()
-                  : FirebaseFirestore.instance
-                  .collection('students')
-                  .where('selected_teacher', isEqualTo: currentUserEmail)
-                  .where('section', isEqualTo: _selectedSection)
-                  .where('year_level', isEqualTo: _selectedYearLevel)
-                  .orderBy('last_name') // Sort by last name
-                  .snapshots(),
+                      ? FirebaseFirestore.instance
+                          .collection('students')
+                          .where('selected_teacher',
+                              isEqualTo: currentUserEmail)
+                          .where('year_level', isEqualTo: _selectedYearLevel)
+                          .orderBy('last_name') // Sort by last name
+                          .snapshots()
+                      : _selectedYearLevel == 0
+                          ? FirebaseFirestore.instance
+                              .collection('students')
+                              .where('selected_teacher',
+                                  isEqualTo: currentUserEmail)
+                              .where('section', isEqualTo: _selectedSection)
+                              .orderBy('last_name') // Sort by last name
+                              .snapshots()
+                          : FirebaseFirestore.instance
+                              .collection('students')
+                              .where('selected_teacher',
+                                  isEqualTo: currentUserEmail)
+                              .where('section', isEqualTo: _selectedSection)
+                              .where('year_level',
+                                  isEqualTo: _selectedYearLevel)
+                              .orderBy('last_name') // Sort by last name
+                              .snapshots(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
@@ -124,7 +160,8 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
                             builder: (BuildContext context) {
                               return AlertDialog(
                                 title: Text('Unenroll Student'),
-                                content: Text('Are you sure you want to unenroll this student?'),
+                                content: Text(
+                                    'Are you sure you want to unenroll this student?'),
                                 actions: <Widget>[
                                   TextButton(
                                     child: Text('No'),
@@ -135,7 +172,8 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
                                   TextButton(
                                     child: Text('Yes'),
                                     onPressed: () {
-                                      Navigator.of(context).pop(); // Close current dialog
+                                      Navigator.of(context)
+                                          .pop(); // Close current dialog
                                       showDialog(
                                         context: context,
                                         builder: (BuildContext context) {
@@ -156,15 +194,25 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
                                                   FirebaseFirestore.instance
                                                       .collection('students')
                                                       .doc(studentData.id)
-                                                      .update({'selected_teacher': null})
-                                                      .then((_) {
-                                                    ScaffoldMessenger.of(context).showSnackBar(
-                                                      SnackBar(content: Text('Student unenrolled successfully')),
+                                                      .update({
+                                                    'selected_teacher': null
+                                                  }).then((_) {
+                                                    ScaffoldMessenger.of(
+                                                            context)
+                                                        .showSnackBar(
+                                                      SnackBar(
+                                                          content: Text(
+                                                              'Student unenrolled successfully')),
                                                     );
-                                                    Navigator.of(context).pop(); // Close current dialog
+                                                    Navigator.of(context)
+                                                        .pop(); // Close current dialog
                                                   }).catchError((error) {
-                                                    ScaffoldMessenger.of(context).showSnackBar(
-                                                      SnackBar(content: Text('Failed to unenroll student: $error')),
+                                                    ScaffoldMessenger.of(
+                                                            context)
+                                                        .showSnackBar(
+                                                      SnackBar(
+                                                          content: Text(
+                                                              'Failed to unenroll student: $error')),
                                                     );
                                                   });
                                                 },
@@ -183,9 +231,11 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
                         title: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('Name: ${studentData['last_name']}, ${studentData['first_name']} ${studentData['middle_initial'] ?? ''}.'),
+                            Text(
+                                'Name: ${studentData['last_name']}, ${studentData['first_name']} ${studentData['middle_initial'] ?? ''}.'),
                             SizedBox(height: 4),
-                            Text('Student Number: ${studentData['student_number']}'),
+                            Text(
+                                'Student Number: ${studentData['student_number']}'),
                             Text('Year Level: ${studentData['year_level']}'),
                             Text('Section: ${studentData['section']}'),
                             Text('Email: ${studentData['email']}'),
@@ -200,6 +250,6 @@ class _StudentBottomScreenState extends State<StudentBottomScreen> {
           ),
         ],
       ),
-    );
+    ]));
   }
 }
