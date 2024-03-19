f9e9763cd248e35f95efd59d9d575b26f694d6cc
Hanna RIzza Malana <hannamalana98@gmail.com>
2024-03-19 17:37:07 +0800

    professor categorize only no design yet

--- lib/admin_profile/admin_bottom/professor_account_bottom.dart
+++ lib/admin_profile/admin_bottom/professor_account_bottom.dart
@@ -27,10 +27,15 @@ class _AdminBottomProfessorAccountStateState
                 DocumentSnapshot<Map<String, dynamic>>? professor =
                     querySnapshot?.docs[index];
                 if (professor != null) {
-                  return ListTile(
+
+
+
+                return ListTile(
+
                       title: Container(
                           padding: EdgeInsets.all(8),
-                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+
                           decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(8),
@@ -59,139 +64,27 @@ class _AdminBottomProfessorAccountStateState
                                   ],
                                 ),
                               ),
-                              SizedBox(width: 10),
+
+                              //Delete Text beside the name inside the container
+                              SizedBox(width: 10), // width
                               Container(
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(8),
                                     color: Colors
-                                        .transparent, // You can change the color here
+                                        .transparent, //Delete box container color
                                   ),
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Delete Professor"),
-                                            content: SingleChildScrollView(
-                                              child: Column(
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
-                                                children: [
-                                                  Text(
-                                                      "Are you sure you want to delete this professor?"),
-                                                  SizedBox(height: 10),
-                                                  TextField(
-                                                    controller:
-                                                        _passwordController,
-                                                    obscureText: true,
-                                                    decoration: InputDecoration(
-                                                      labelText:
-                                                          'Enter Admin Password',
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                            actions: [
-                                              TextButton(
-                                                onPressed: () {
-                                                  Navigator.of(context).pop(
-                                                      null); // Close dialog with null result
-                                                  _passwordController
-                                                      .clear(); // Clear password field
-                                                },
-                                                child: Text("Cancel"),
-                                              ),
-                                              TextButton(
-                                                onPressed: () {
-                                                  String password =
-                                                      _passwordController.text;
-                                                  if (password ==
-                                                      'Admin@L1b3rtad') {
-                                                    showDialog(
-                                                      context: context,
-                                                      builder: (BuildContext
-                                                          context) {
-                                                        return AlertDialog(
-                                                          title: Text(
-                                                              "Confirm Deletion"),
-                                                          content: Text(
-                                                              "This action cannot be undone. Are you sure?"),
-                                                          actions: [
-                                                            TextButton(
-                                                              onPressed:
-                                                                  () async {
-                                                                await FirebaseFirestore
-                                                                    .instance
-                                                                    .collection(
-                                                                        'professor_instructor')
-                                                                    .doc(
-                                                                        professor
-                                                                            .id)
-                                                                    .delete();
-                                                                ScaffoldMessenger.of(
-                                                                        context)
-                                                                    .showSnackBar(
-                                                                  SnackBar(
-                                                                    content: Text(
-                                                                        "Professor deleted successfully"),
-                                                                  ),
-                                                                );
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop();
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop();
-                                                              },
-                                                              child:
-                                                                  Text("Yes"),
-                                                            ),
-                                                            TextButton(
-                                                              onPressed: () {
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop();
-                                                              },
-                                                              child: Text("No"),
-                                                            ),
-                                                          ],
-                                                        );
-                                                      },
-                                                    );
-                                                  } else {
-                                                    ScaffoldMessenger.of(
-                                                            context)
-                                                        .showSnackBar(
-                                                      SnackBar(
-                                                        content: Text(
-                                                            "Incorrect admin password"),
-                                                      ),
-                                                    );
-                                                  }
-                                                },
-                                                child: Text("Delete"),
-                                              ),
-                                            ],
-                                          );
-                                        },
-                                      ).then((value) {
-                                        // Clear password field when dialog is dismissed
-                                        _passwordController.clear();
-                                      });
+                                  child: InkWell(
+                                    onTap: () {
+                                      deleteConfirmationDialog(context, professor);
                                     },
-                                    style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(Colors
-                                              .transparent), // Change background color to red
-                                    ),
                                     child: Text(
                                       "Delete",
-                                      style: TextStyle(color: Colors.white),
+                                      style: TextStyle(color: Colors.red),
                                     ),
                                   ))
-                            ],
+
+
+                              ],
                           )));
                 } else {
                   return SizedBox(); // Return an empty widget if professor is null
@@ -207,4 +100,101 @@ class _AdminBottomProfessorAccountStateState
       ),
     );
   }
+
+
+void deleteConfirmationDialog(
+    BuildContext context, DocumentSnapshot<Map<String, dynamic>> professor) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Delete Professor"),
+        content: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Are you sure you want to delete this professor?"),
+              SizedBox(height: 10),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Enter Admin Password',
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(null);
+              _passwordController.clear();
+            },
+            child: Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              String password = _passwordController.text;
+              if (password == 'Admin@L1b3rtad') {
+                confirmDeletionDialog(context, professor);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Incorrect admin password"),
+                  ),
+                );
+              }
+            },
+            child: Text("Delete",
+      style: TextStyle(color: Colors.red),
+          ),
+          )],
+      );
+    },
+  ).then((value) {
+    _passwordController.clear();
+  });
 }
+
+void confirmDeletionDialog(
+    BuildContext context, DocumentSnapshot<Map<String, dynamic>> professor) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Confirm Deletion"),
+        content: Text("This action cannot be undone. Are you sure?"),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await FirebaseFirestore.instance
+                  .collection('professor_instructor')
+                  .doc(professor.id)
+                  .delete();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Professor deleted successfully"),
+                ),
+              );
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+            child: Text("Yes"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("No"),
+          ),
+        ],
+      );
+    },
+  );
+}
+}
+
+
+
+
